0b0115705cdb8912e79f5754fb9fc16780654d05
Adam Tal <adamtal3@gmail.com>
2015-12-26 14:41:35 +0200

    Updated icon and implemented notification icon and close confirmation

--- PunchClock/Main.Designer.cs
+++ PunchClock/Main.Designer.cs
@@ -28,19 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.btnSave = new MaterialSkin.Controls.MaterialRaisedButton();
             this.btnCancel = new MaterialSkin.Controls.MaterialFlatButton();
+            this.niNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
             // 
             // btnSave
             // 
             this.btnSave.Depth = 0;
-            this.btnSave.Location = new System.Drawing.Point(408, 348);
+            this.btnSave.Location = new System.Drawing.Point(398, 348);
             this.btnSave.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnSave.Name = "btnSave";
             this.btnSave.Primary = true;
-            this.btnSave.Size = new System.Drawing.Size(80, 40);
+            this.btnSave.Size = new System.Drawing.Size(90, 40);
             this.btnSave.TabIndex = 0;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -50,16 +52,24 @@
             // 
             this.btnCancel.Depth = 0;
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(321, 348);
+            this.btnCancel.Location = new System.Drawing.Point(291, 348);
             this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.btnCancel.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Primary = false;
-            this.btnCancel.Size = new System.Drawing.Size(80, 40);
+            this.btnCancel.Size = new System.Drawing.Size(100, 40);
             this.btnCancel.TabIndex = 1;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // niNotifyIcon
+            // 
+            this.niNotifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.niNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("niNotifyIcon.Icon")));
+            this.niNotifyIcon.Text = "Punch Clock";
+            this.niNotifyIcon.Visible = true;
+            this.niNotifyIcon.Click += new System.EventHandler(this.niNotifyIcon_Click);
             // 
             // Main
             // 
@@ -75,11 +85,12 @@
             this.MaximumSize = new System.Drawing.Size(500, 400);
             this.MinimumSize = new System.Drawing.Size(500, 400);
             this.Name = "Main";
-            this.ShowInTaskbar = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Punch Clock";
             this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Resize += new System.EventHandler(this.Main_Resize);
             this.ResumeLayout(false);
 
         }
@@ -88,6 +99,7 @@
 
         private MaterialSkin.Controls.MaterialFlatButton btnCancel;
         private MaterialSkin.Controls.MaterialRaisedButton btnSave;
+        private System.Windows.Forms.NotifyIcon niNotifyIcon;
     }
 }
 
--- PunchClock/Main.cs
+++ PunchClock/Main.cs
@@ -1,4 +1,5 @@
-﻿using MaterialSkin;
+﻿using System.Windows.Forms;
+using MaterialSkin;
 using MaterialSkin.Controls;
 
 namespace PunchClock
@@ -14,17 +15,58 @@ namespace PunchClock
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
             materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+
+            // Notify icon
+            niNotifyIcon.ContextMenu = new ContextMenu(new[] {
+                new MenuItem("Show", niNotifyIcon_Click),
+                new MenuItem("-"), 
+                new MenuItem("Exit", (sender, args) => { this.Close(); })
+            });
         }
 
         private void btnSave_Click(object sender, System.EventArgs e)
         {
             // Save changed to configuration json file
-            Hide();
+            this.WindowState = FormWindowState.Minimized;
         }
 
         private void btnCancel_Click(object sender, System.EventArgs e)
         {
-            Hide();
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Main_Resize(object sender, System.EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                niNotifyIcon.Visible = true;
+                Notify("Punch clock is still running");
+                this.Hide();
+            }
+            else if (this.WindowState == FormWindowState.Normal)
+            {
+                niNotifyIcon.Visible = false;
+            }
+        }
+
+        private void Notify(string text)
+        {
+            niNotifyIcon.BalloonTipText = text;
+            niNotifyIcon.ShowBalloonTip(1500);
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var dialogResult = MessageBox.Show("Are you sure you want to close?", "Exit Confirmation",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogResult != DialogResult.Yes)
+                e.Cancel = true;
+        }
+
+        private void niNotifyIcon_Click(object sender, System.EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
         }
     }
 }
